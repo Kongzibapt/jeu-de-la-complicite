de416d9353e988af963d6bd07e18dfbac4cb6f75
Kongzibapt <baptiste1296@gmail.com>
2025-12-03 23:50:18 +0100

    Ajout data names

--- app/data/names.ts
+++ app/data/names.ts
@@ -282,9 +282,387 @@ const lieux = [
   "Opéra de Sydney",
 ];
 
+const mangas = [
+  "Luffy",
+  "Zoro",
+  "Nami",
+  "Sanji",
+  "Robin",
+  
+  "Naruto",
+  "Sasuke",
+  "Sakura",
+  "Kakashi",
+
+  "Goku",
+  "Vegeta",
+  "Gohan",
+  "Trunks",
+  "Bulma",
+
+  "Tanjiro",
+  "Nezuko",
+  "Zenitsu",
+  "Inosuke",
+
+  "Eren Yeager",
+  "Mikasa",
+  "Armin",
+  "Levi Ackerman",
+
+  "Saitama",
+  "Genos",
+
+  "Light Yagami",
+  "L (Death Note)",
+
+  "Yuji Itadori",
+  "Gojo Satoru",
+  "Megumi Fushiguro",
+];
+
+const creators = [
+  "Squeezie",
+  "Cyprien",
+  "Norman",
+  "McFly et Carlito",
+  "Mister V",
+  "Gotaga",
+  "Inoxtag",
+  "Michou",
+  "Joyca",
+  "LeBouseuh",
+  "Amixem",
+  "Pierre Croce",
+
+  "PewDiePie",
+  "MrBeast",
+  "Ninja",
+  "Pokimane",
+  "Dream",
+  "Markiplier",
+  "Jacksepticeye",
+];
+
+const series = [
+  "Geralt de Riv",
+  "Yennefer",
+  "Ciri",
+
+  "Joe Goldberg",
+  "Love Quinn",
+
+  "Wednesday Addams",
+  "Morticia Addams",
+
+  "Joel (The Last of Us)",
+  "Ellie (The Last of Us)",
+
+  "Homelander",
+  "Starlight",
+  "Billy Butcher",
+
+  "Ragnar Lothbrok",
+  "Lagertha",
+
+  "Michael Scott",
+  "Dwight Schrute",
+  "Jim Halpert",
+  "Pam Beesly",
+
+  "Ted Lasso",
+];
+
+const films = [
+  "Jack Sparrow",
+  "Elizabeth Swann",
+  "Will Turner",
+
+  "Katniss Everdeen",
+  "Peeta Mellark",
+
+  "Shrek",
+  "Donkey",
+
+  "Elsa",
+  "Anna",
+  "Olaf",
+
+  "Lightning McQueen",
+  "Mater",
+
+  "Gru",
+  "Les Minions",
+
+  "Deadpool",
+  "Cable",
+  "Colossus",
+
+  "John Wick",
+];
+
+const artistes2 = [
+  "ROSALÍA",
+  "Travis Scott",
+  "J Balvin",
+  "Karol G",
+
+  "Central Cee",
+  "Dave",
+  "Lil Nas X",
+
+  "SCH",
+  "Naps",
+  "Tiakola",
+  "Gazo",
+  "Zola",
+];
+
+const sport = [
+  // Football
+  "Kylian Mbappé",
+  "Antoine Griezmann",
+  "Lionel Messi",
+  "Cristiano Ronaldo",
+  "Neymar",
+  "Karim Benzema",
+  "Erling Haaland",
+  "Zinedine Zidane",
+  "Didier Deschamps",
+
+  // Basket
+  "LeBron James",
+  "Stephen Curry",
+  "Michael Jordan",
+  "Kobe Bryant",
+  "Giannis Antetokounmpo",
+
+  // Tennis
+  "Serena Williams",
+  "Roger Federer",
+  "Rafael Nadal",
+  "Novak Djokovic",
+  "Naomi Osaka",
+  "Carlos Alcaraz",
+
+  // Sports mécaniques
+  "Lewis Hamilton",
+  "Max Verstappen",
+  "Fernando Alonso",
+
+  // Rugby
+  "Antoine Dupont",
+  "Jonah Lomu",
+  "Sébastien Chabal",
+
+  // Autres sports connus
+  "Usain Bolt",
+  "Tony Parker",
+  "Shaquille O'Neal",
+  "Conor McGregor",
+];
+
+const apps = [
+  "Instagram",
+  "TikTok",
+  "YouTube",
+  "Snapchat",
+  "Twitter",
+  "WhatsApp",
+  "Messenger",
+  "Discord",
+  "Twitch",
+
+  "Netflix",
+  "Disney+",
+  "Prime Video",
+
+  "Spotify",
+  "Deezer",
+
+  "Google Maps",
+  "Waze",
+  "Uber",
+  "Uber Eats",
+  "Deliveroo",
+  "Airbnb",
+  "Doctolib",
+
+  "Amazon",
+  "Vinted",
+  "Leboncoin",
+
+  "LinkedIn",
+  "Gmail",
+  "Google Drive",
+];
+
+const objets = [
+  "iPhone",
+  "AirPods",
+  "Nintendo Switch",
+  "PS5",
+  "Xbox",
+  "GoPro",
+
+  "Drone",
+  "Casque VR",
+  "Tondeuse barbe",
+  "Brosse à dents électrique",
+  "Micro-onde",
+  "Aspirateur robot",
+];
+
+const food = [
+  "Nutella",
+  "McDonald's",
+  "Burger King",
+  "Starbucks",
+  "KFC",
+  "Domino's Pizza",
+  "Ben & Jerry's",
+  "Coca-Cola",
+  "Pepsi",
+  "Kinder Bueno",
+  "Haribo",
+];
+
+const marques = [
+  // Sport / streetwear
+  "Nike",
+  "Adidas",
+  "Puma",
+  "New Balance",
+  "Converse",
+  "Vans",
+  "Reebok",
+  "The North Face",
+  "Patagonia",
+
+  // Mode grand public
+  "Zara",
+  "H&M",
+  "Uniqlo",
+  "Pull&Bear",
+  "Bershka",
+  "Celio",
+  "Levi's",
+
+  // Luxe / hype
+  "Supreme",
+  "Gucci",
+  "Louis Vuitton",
+  "Balenciaga",
+  "Off-White",
+
+  // Tech / lifestyle
+  "Apple",
+  "Samsung",
+  "IKEA",
+  "Decathlon",
+  "Lacoste",
+];
+
+const popCulture2 = [
+  "Megamind",
+  "Baymax",
+  "Ralph la Casse",
+  "Miraculous Ladybug",
+  "Chat Noir",
+
+  "Jack-Jack",
+  "Elastigirl",
+  "Mr Indestructible",
+
+  "Harvey Specter",
+  "Mike Ross",
+  "Donna Paulsen",
+  "Louis Litt",
+
+  "Katara",
+  "Aang",
+  "Zuko",
+  "Toph Beifong",
+];
+
+const jeuxVideo2 = [
+  "Kratos",
+  "Atreus",
+  "Ellie Williams",
+
+  "Agent 47",
+  "Cloud Strife",
+  "Sephiroth",
+
+  "Pac-Man",
+  "Kirby",
+  "Samus Aran",
+
+  "Tracer",
+  "Widowmaker",
+  "Ashe",
+
+  "Viper (Valorant)",
+  "Jett",
+  "Sage",
+  "Omen",
+];
+
+const lieux2 = [
+  "Séoul",
+  "Pékin",
+  "Shanghai",
+  "Dubaï",
+  "Le Caire",
+  "Marrakech",
+
+  "Times Square",
+  "Walt Disney World",
+  "Universal Studios",
+
+  "Le Louvre",
+  "La Défense",
+
+  "Mont Fuji",
+];
+
+
 export default [
   ...popCulture,
+  ...popCulture2,
   ...musique,
+  ...artistes2,
   ...jeuxVideo,
-  ...lieux
+  ...jeuxVideo2,
+  ...mangas,
+  ...series,
+  ...films,
+  ...creators,
+  ...sport,
+  ...apps,
+  ...objets,
+  ...food,
+  ...marques,
+  ...lieux,
+  ...lieux2,
 ];
+
+console.log([
+  ...popCulture,
+  ...popCulture2,
+  ...musique,
+  ...artistes2,
+  ...jeuxVideo,
+  ...jeuxVideo2,
+  ...mangas,
+  ...series,
+  ...films,
+  ...creators,
+  ...sport,
+  ...apps,
+  ...objets,
+  ...food,
+  ...marques,
+  ...lieux,
+  ...lieux2,
+].length)
