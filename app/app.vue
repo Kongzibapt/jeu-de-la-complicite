368eb110f821adf030b0be5b77846ed7bd715456
Kongzibapt <baptiste1296@gmail.com>
2025-12-03 23:34:01 +0100

    Ajout verification Google Search Console

--- app/app.vue
+++ app/app.vue
@@ -7,6 +7,29 @@ useHead({
     }
   ]
 });
+
+useHead({
+  script: [
+    {
+      async: true,
+      src: "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX",
+    }
+  ]
+});
+
+useHead({
+  script: [
+    {
+      innerHTML: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-7VFCVYWWEN');
+      `
+    }
+  ]
+});
+
 </script>
 
 
