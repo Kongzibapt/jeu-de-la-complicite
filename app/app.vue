18acae3664848ed6da05dfacdf330efec87ed686
Kongzibapt <baptiste1296@gmail.com>
2025-12-16 22:13:06 +0100

    Modif logo

--- app/app.vue
+++ app/app.vue
@@ -4,8 +4,18 @@ useHead({
     {
       name: "google-site-verification",
       content: "8wBBZjp7946Sw8-oH7UhKG6jsU4jI6jQB-AIxMGqDig"
-    }
-  ]
+    },
+    { property: "og:image", content: "https://jeu-de-la-complicite.fr/og-image.png" },
+    { property: "og:image:width", content: "1200" },
+    { property: "og:image:height", content: "630" },
+    { property: "og:type", content: "website" },
+  ],
+  link: [
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+    { rel: "apple-touch-icon", sizes: "192x192", href: "/favicon-192.png" },
+    { rel: "manifest", href: "/manifest.json" }
+  ],
 });
 
 useHead({
